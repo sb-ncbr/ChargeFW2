17309eb52ebf19be11c403b96680b13b2731fd12
Tomáš Raček <tom@krab1k.net>
2018-12-06 14:11:29 +0100

    Add SFKEEM method.

---
+++ src/methods/sfkeem.cpp
@@ -0,0 +1,65 @@
+//
+// Created by krab1k on 31/10/18.
+//
+
+#include <vector>
+#include <cmath>
+#include <mkl_lapacke.h>
+#include <mkl.h>
+
+#include "sfkeem.h"
+#include "../parameters.h"
+
+
+double distance(const Atom &atom1, const Atom &atom2) {
+    double dx = atom1.pos()[0] - atom2.pos()[0];
+    double dy = atom1.pos()[1] - atom2.pos()[1];
+    double dz = atom1.pos()[2] - atom2.pos()[2];
+
+    return std::sqrt(dx * dx + dy * dy + dz * dz);
+}
+
+#define IDX(i, j) (i * m + j)
+
+std::vector<double> SFKEEM::calculate_charges(const Molecule &molecule) {
+
+    size_t n = molecule.atoms().size();
+    size_t m = n + 1;
+
+    auto *A = (double *) mkl_malloc(m * m * sizeof(double), 64);
+    auto *b = (double *) mkl_malloc(m * sizeof(double), 64);
+    auto *ipiv = (MKL_INT *) mkl_malloc(m * sizeof(MKL_INT), 64);
+
+    for (size_t i = 0; i < n; i++) {
+        const auto &atom_i = molecule.atoms()[i];
+        A[IDX(i, i)] = 2 * parameters_->atom()->parameter(atom::B)(atom_i);
+        b[i] = -parameters_->atom()->parameter(atom::A)(atom_i);
+        for (size_t j = i + 1; j < n; j++) {
+            const auto &atom_j = molecule.atoms()[j];
+            A[IDX(i, j)] = 2 * sqrt(parameters_->atom()->parameter(atom::B)(atom_i) *
+                                    parameters_->atom()->parameter(atom::B)(atom_j)) /
+                           cosh(parameters_->common()->parameter(common::sigma) * distance(atom_i, atom_j));
+        }
+    }
+
+    for (size_t i = 0; i < n; i++) {
+        A[IDX(i, n)] = 1;
+    }
+
+    A[IDX(n, n)] = 0;
+    b[n] = 0;
+
+    MKL_INT info = LAPACKE_dsysv(LAPACK_ROW_MAJOR, 'U', m, 1, A, m, ipiv, b, 1);
+    if (info) {
+        throw std::runtime_error("Cannot solve linear system");
+    }
+
+    std::vector<double> results;
+    results.assign(b, b + n);
+
+    mkl_free(A);
+    mkl_free(b);
+    mkl_free(ipiv);
+
+    return results;
+}
---
+++ src/methods/sfkeem.h
@@ -0,0 +1,23 @@
+//
+// Created by krab1k on 31/10/18.
+//
+
+#pragma once
+
+#include <vector>
+#include <boost/config.hpp>
+
+#include "../structures/molecule.h"
+#include "../method.h"
+
+class SFKEEM : public Method {
+    enum common{sigma};
+    enum atom{A, B};
+public:
+    explicit SFKEEM() : Method("SFKEEM", {"sigma"}, {"A", "B"}, {}) {}
+
+    std::vector<double> calculate_charges(const Molecule &molecule) override;
+};
+
+extern "C" BOOST_SYMBOL_EXPORT SFKEEM method;
+SFKEEM method;
