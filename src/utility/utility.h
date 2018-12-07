2d386d8a028446566b03fa82f57bf49e3f4c7fcb
Tomáš Raček <tom@krab1k.net>
2018-12-07 13:23:13 +0100

    Use fixed format when printing vector of charges.

--- src/utility/utility.h
+++ src/utility/utility.h
@@ -5,10 +5,12 @@
 #pragma once
 
 #include <iostream>
+#include <iomanip>
 #include <vector>
 
 template<typename T>
 std::ostream &operator<<(std::ostream &os, const std::vector<T> &vec) {
+    os << std::fixed << std::setprecision(3);
     for (const auto &val: vec) {
         os << val << " ";
     }
