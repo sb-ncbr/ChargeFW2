a4da8fafccfa2f588942dc44ffff39364864c8fc
Tomáš Raček <tom@krab1k.net>
2018-12-13 17:08:47 +0100

    Add DENR.

---
+++ src/methods/denr.cpp
@@ -0,0 +1,65 @@
+//
+// Created by krab1k on 31/10/18.
+//
+
+#include <vector>
+#include <cmath>
+#include <mkl_lapacke.h>
+#include <mkl.h>
+
+#include "denr.h"
+#include "../structures/molecule.h"
+
+
+std::vector<double> DENR::calculate_charges(const Molecule &molecule) {
+
+    size_t n = molecule.atoms().size();
+
+    auto *L = (double *) mkl_calloc(n * n, sizeof(double), 64);
+    auto *chi = (double *) mkl_calloc(n, sizeof(double), 64);
+    auto *q = (double *) mkl_calloc(n, sizeof(double), 64);
+    auto *ipiv = (lapack_int *) mkl_calloc(n, sizeof(lapack_int), 64);
+    auto *tmp = (double *) mkl_calloc(n, sizeof(double), 64);
+    auto *I = (double *) mkl_calloc(n * n, sizeof(double), 64);
+    auto *eta = (double *) mkl_calloc(n * n, sizeof(double), 64);
+
+    for (size_t i = 0; i < n; i++) {
+        auto &atom_i = molecule.atoms()[i];
+        chi[i] = parameters_->atom()->parameter(atom::electronegativity)(atom_i);
+        eta[i * n + i] = parameters_->atom()->parameter(atom::hardness)(atom_i);
+        I[i * n + i] = 1.0;
+        L[i * n + i] = molecule.degree(atom_i);
+        for (size_t j = i + 1; j < n; j++) {
+            auto &atom_j = molecule.atoms()[j];
+            if (molecule.bonded(atom_i, atom_j)) {
+                L[i * n + j] = -1;
+            }
+        }
+    }
+
+    double step = parameters_->common()->parameter(common::step);
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n, step, L, n, eta, n, 1.0, I, n);
+    LAPACKE_dgetrf(LAPACK_ROW_MAJOR, n, n, I, n, ipiv);
+
+    cblas_dgemv(CblasRowMajor, CblasNoTrans, n, n, step, L, n, chi, 1, 0, tmp, 1);
+
+    for (int i = 0; i < parameters_->common()->parameter(common::iterations); i++) {
+        cblas_daxpby(n, -1.0, tmp, 1, 1.0, q, 1);
+        LAPACKE_dgetrs(LAPACK_ROW_MAJOR, 'N', n, 1, I, n, ipiv, q, 1);
+    }
+
+    std::vector<double> res(n, 0);
+    for(size_t i = 0; i < n; i++) {
+        res[i] = q[i];
+    }
+
+    mkl_free(L);
+    mkl_free(chi);
+    mkl_free(ipiv);
+    mkl_free(q);
+    mkl_free(eta);
+    mkl_free(tmp);
+    mkl_free(I);
+
+    return res;
+}
---
+++ src/methods/denr.h
@@ -0,0 +1,23 @@
+//
+// Created by krab1k on 31/10/18.
+//
+
+#pragma once
+
+#include <vector>
+#include <boost/config.hpp>
+
+#include "../structures/molecule.h"
+#include "../method.h"
+
+class DENR : public Method {
+    enum common{step, iterations};
+    enum atom{electronegativity, hardness};
+public:
+    explicit DENR() : Method("DENR", {"step", "iterations"}, {"electronegativity", "hardness"}, {}) {}
+
+    std::vector<double> calculate_charges(const Molecule &molecule) override;
+};
+
+extern "C" BOOST_SYMBOL_EXPORT DENR method;
+DENR method;
