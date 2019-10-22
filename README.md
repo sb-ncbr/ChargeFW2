978b2a5a541debbf0ca893b883bb68327cdee1bf
Tomáš Raček <tom@krab1k.net>
2019-10-22 15:56:56 +0200

    [formats/mmcif] Use GEMMI library for parsing

--- README.md
+++ README.md
@@ -3,11 +3,12 @@
 Application for computing atomic charges using selected empirical methods.
 
 ## Compilation requirements
-- [CMake](https://cmake.org/) > 3.9
-- [GCC](https://gcc.gnu.org/) > 8.2 or [Clang](https://clang.llvm.org/) > 8
-- [Boost](https://www.boost.org/) > 1.65
+- [CMake](https://cmake.org/) >= 3.9
+- [GCC](https://gcc.gnu.org/) >= 8.2 or [Clang](https://clang.llvm.org/) >= 8
+- [Boost](https://www.boost.org/) >= 1.65
 - [Eigen](http://eigen.tuxfamily.org) 3.3
-- [fmt](https://fmt.dev) > 5.3.0
+- [fmt](https://fmt.dev) >= 5.3.0
 - [nanoflann](https://github.com/jlblancoc/nanoflann) 1.3.0
-- [NLopt](https://github.com/stevengj/nlopt) > 2.4.2
-- [JSON for Modern C++](https://github.com/nlohmann/json) > 3.6.1
+- [NLopt](https://github.com/stevengj/nlopt) >= 2.4.2
+- [JSON for Modern C++](https://github.com/nlohmann/json) >= 3.6.1
+- [GEMMI](https://github.com/project-gemmi/gemmi) > =0.3.1
--- src/formats/mmcif.cpp
+++ src/formats/mmcif.cpp
@@ -9,6 +9,8 @@
 #include <fstream>
 #include <fmt/format.h>
 #include <boost/algorithm/string.hpp>
+#include <gemmi/cif.hpp>
+#include <gemmi/mmcif.hpp>
 
 #include "chargefw2.h"
 #include "../config.h"
@@ -18,261 +20,101 @@
 #include "../periodic_table.h"
 
 
-const std::set<std::string> required_protein_fields = {"Cartn_x", "Cartn_y", "Cartn_z", "type_symbol",
-                                                       "label_atom_id", "label_alt_id", "label_seq_id",
-                                                       "auth_seq_id", "label_comp_id", "label_asym_id",
-                                                       "pdbx_PDB_model_num"};
+void read_protein_molecule(gemmi::cif::Block &data, std::unique_ptr<std::vector<Atom>> &atoms);
 
-const std::set<std::string> required_ccd_fields = {"model_Cartn_x", "model_Cartn_y", "model_Cartn_z", "type_symbol",
-                                                   "atom_id", "comp_id"};
+void read_ccd_molecule(gemmi::cif::Block &data, std::unique_ptr<std::vector<Atom>> &atoms);
 
 
-bool has_required_keys(const std::map<std::string, size_t> &fields, const std::set<std::string> &required,
-                       std::string &missing);
+void read_protein_molecule(gemmi::cif::Block &data, std::unique_ptr<std::vector<Atom>> &atoms) {
+    auto structure = gemmi::make_structure_from_block(data);
+    if (structure.models.empty()) {
+        fmt::print(stderr, "Not enough information to create a structure from {}\n", data.name);
+        exit(EXIT_FILE_ERROR);
+    }
 
-void
-read_protein_molecule(std::ifstream &file, const std::string &name, std::unique_ptr<std::vector<Molecule>> &molecules);
+    /* Read first model only */
+    auto model = structure.models[0];
+    size_t idx = 0;
+    for (const auto &chain: model.chains) {
+        for (const auto &residue: chain.residues) {
+            bool hetatm = residue.het_flag == 'H';
+            for (const auto &atom: residue.atoms) {
+                double x = atom.pos.x;
+                double y = atom.pos.y;
+                double z = atom.pos.z;
+                int residue_id = hetatm ? residue.seqid.num.value : residue.label_seq.value;
+                auto element = PeriodicTable::pte().get_element_by_symbol(get_element_symbol(atom.element.name()));
 
-void read_ccd_molecule(std::ifstream &file, const std::string &name, std::unique_ptr<std::vector<Molecule>> &molecules);
+                if (not atom.has_altloc() or not is_already_loaded(*atoms, atom.name, residue_id)) {
+                    if ((not hetatm) or
+                        (config::read_hetatm and residue.name != "HOH") or
+                        (config::read_hetatm and not config::ignore_water)) {
+                        atoms->emplace_back(idx, element, x, y, z, atom.name, residue_id, residue.name, chain.name, hetatm);
+                        idx++;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+void read_ccd_molecule(gemmi::cif::Block &data, std::unique_ptr<std::vector<Atom>> &atoms) {
+    auto table = data.find("_chem_comp_atom.", {"model_Cartn_x", "model_Cartn_y", "model_Cartn_z",
+                                                "type_symbol", "atom_id", "comp_id"});
+    size_t idx = 0;
+    for (const auto row: table) {
+        double x, y, z;
+        try {
+            x = std::stod(row[0]);
+            y = std::stod(row[0]);
+            z = std::stod(row[0]);
+        } catch (std::exception &) {
+            fmt::print(stderr, "Cannot load coordinates for {}\n", data.name);
+            exit(EXIT_FILE_ERROR);
+        }
+
+        auto element = PeriodicTable::pte().get_element_by_symbol(get_element_symbol(row[3]));
+        auto atom_name = row[4];
+        auto residue = row[5];
+        auto residue_id = 0;
+
+        atoms->emplace_back(idx, element, x, y, z, atom_name, residue_id, residue, "0", false);
+        idx++;
+    }
+}
 
 
 MoleculeSet mmCIF::read_file(const std::string &filename) {
-    std::ifstream file(filename);
-    if (!file) {
+
+    gemmi::cif::Document doc;
+    try {
+         doc = gemmi::cif::read_file(filename);
+    }
+    catch (std::exception &){
         fmt::print(stderr, "Cannot open file: {}\n", filename);
         exit(EXIT_FILE_ERROR);
     }
 
-    auto molecules = std::make_unique<std::vector<Molecule> >();
+    auto molecules = std::make_unique<std::vector<Molecule>>();
+    for (auto &data: doc.blocks) {
+        const auto names = data.get_mmcif_category_names();
 
-    std::string line;
-    std::string name;
-    try {
-        while (std::getline(file, line)) {
-            if (boost::starts_with(line, "_entry.id")) {
-                std::stringstream ss(line);
-                ss >> name >> name;
-                read_protein_molecule(file, sanitize_name(name), molecules);
-                break;
-            }
-
-            if (boost::starts_with(line, "_chem_comp.id")) {
-                std::stringstream ss(line);
-                ss >> name >> name;
-                read_ccd_molecule(file, sanitize_name(name), molecules);
-                break;
-            }
+        auto atoms = std::make_unique<std::vector<Atom>>();
+        if (std::find(names.begin(), names.end(), "_atom_site.") != names.end()) {
+            read_protein_molecule(data, atoms);
+        } else if (std::find(names.begin(), names.end(), "_chem_comp_atom.") != names.end()) {
+            read_ccd_molecule(data, atoms);
         }
-    } catch (const std::exception &) {
-        fmt::print(stderr, "Invalid mmCIF file\n");
-        exit(EXIT_FILE_ERROR);
-    }
 
+        if (atoms->empty()) {
+            fmt::print(stderr, "No atoms were loaded from the input file.\n");
+            exit(EXIT_FILE_ERROR);
+        }
+
+        auto bonds = get_bonds(atoms);
+        std::map<size_t, int> charges;
+        molecules->emplace_back(data.name, std::move(atoms), std::move(bonds), charges);
+    }
     return MoleculeSet(std::move(molecules));
-}
-
-
-bool has_required_keys(const std::map<std::string, size_t> &fields, const std::set<std::string> &required,
-                       std::string &missing) {
-    for (const auto &req_field: required) {
-        if (fields.find(req_field) == fields.end()) {
-            missing = req_field;
-            return false;
-        }
-    }
-    return true;
-}
-
-
-void
-read_protein_molecule(std::ifstream &file, const std::string &name, std::unique_ptr<std::vector<Molecule>> &molecules) {
-
-    std::string line;
-    while (std::getline(file, line)) {
-        if (boost::starts_with(line, "_atom_site.")) {
-            break;
-        }
-    }
-
-    auto atoms = std::make_unique<std::vector<Atom>>();
-    std::map<std::string, size_t> record_positions;
-    size_t category_idx = 0;
-    do {
-        auto it = line.find('.');
-        auto category = line.substr(it + 1);
-        boost::trim(category);
-
-        record_positions[category] = category_idx;
-        category_idx++;
-        std::getline(file, line);
-    } while (boost::starts_with(line, "_atom_site."));
-
-    std::string missing;
-    if (not has_required_keys(record_positions, required_protein_fields, missing)) {
-        fmt::print(stderr, "mmCIF file does not contain required field: _atom_site.{}\n", missing);
-        exit(EXIT_FILE_ERROR);
-    }
-
-    size_t idx = 0;
-
-    int first_model_no = -1;
-    do {
-        std::stringstream ss(line);
-        std::vector<std::string> records{std::istream_iterator<std::string>(ss),
-                                         std::istream_iterator<std::string>{}};
-
-        bool hetatm = false;
-        if (line[0] == 'H') {
-            hetatm = true;
-        }
-
-        double x, y, z;
-
-        try {
-            auto it = record_positions.find("Cartn_x");
-            x = std::stod(records[it->second]);
-
-            it = record_positions.find("Cartn_y");
-            y = std::stod(records[it->second]);
-
-            it = record_positions.find("Cartn_z");
-            z = std::stod(records[it->second]);
-        } catch (std::exception &) {
-            fmt::print(stderr, "Error reading coordinates.\n");
-            exit(EXIT_FILE_ERROR);
-        }
-
-        auto it = record_positions.find("type_symbol");
-        auto symbol = records[it->second];
-        auto element = PeriodicTable::pte().get_element_by_symbol(get_element_symbol(symbol));
-
-        it = record_positions.find("label_atom_id");
-        auto atom_name = fix_atom_name(records[it->second]);
-
-        it = record_positions.find("label_alt_id");
-        auto alt_id = records[it->second];
-
-        int residue_id;
-        /* HETATM record does not have label_seq_id */
-        if (not hetatm) {
-            it = record_positions.find("label_seq_id");
-            residue_id = std::stoi(records[it->second]);
-        } else {
-            it = record_positions.find("auth_seq_id");
-            residue_id = std::stoi(records[it->second]);
-        }
-
-        it = record_positions.find("label_comp_id");
-        auto residue = records[it->second];
-
-        it = record_positions.find("label_asym_id");
-        auto chain_id = records[it->second];
-
-        it = record_positions.find("pdbx_PDB_model_num");
-        auto model_no = std::stoi(records[it->second]);
-
-        if (first_model_no == -1) {
-            first_model_no = model_no;
-        } else {
-            if (first_model_no < model_no) {
-                /* We are currently reading atom from another model */
-                break;
-            }
-        }
-
-        if (alt_id == "." or not is_already_loaded(*atoms, atom_name, residue_id)) {
-            if ((not hetatm) or
-                (config::read_hetatm and residue != "HOH") or
-                (config::read_hetatm and not config::ignore_water)) {
-                atoms->emplace_back(idx, element, x, y, z, atom_name, residue_id, residue, chain_id, hetatm);
-                idx++;
-            }
-        }
-
-        std::getline(file, line);
-        boost::trim(line);
-    } while (line[0] != '#');
-
-    if (atoms->empty()) {
-        fmt::print(stderr, "No atoms were loaded from the input file.\n");
-        exit(EXIT_FILE_ERROR);
-    }
-
-    auto bonds = get_bonds(atoms);
-    std::map<size_t, int> charges;
-    molecules->emplace_back(name, std::move(atoms), std::move(bonds), charges);
-}
-
-
-void
-read_ccd_molecule(std::ifstream &file, const std::string &name, std::unique_ptr<std::vector<Molecule>> &molecules) {
-    std::string line;
-    while (std::getline(file, line)) {
-        if (boost::starts_with(line, "_chem_comp_atom.")) {
-            break;
-        }
-    }
-
-    auto atoms = std::make_unique<std::vector<Atom>>();
-    std::map<std::string, size_t> record_positions;
-    size_t category_idx = 0;
-    do {
-        auto it = line.find('.');
-        auto category = line.substr(it + 1);
-        boost::trim(category);
-
-        record_positions[category] = category_idx;
-        category_idx++;
-        std::getline(file, line);
-    } while (boost::starts_with(line, "_chem_comp_atom."));
-
-    std::string missing;
-    if (not has_required_keys(record_positions, required_ccd_fields, missing)) {
-        fmt::print(stderr, "mmCIF file does not contain required field: _chem_comp_atom.{}\n", missing);
-        exit(EXIT_FILE_ERROR);
-    }
-
-    size_t idx = 0;
-    do {
-        std::stringstream ss(line);
-        std::vector<std::string> records{std::istream_iterator<std::string>(ss),
-                                         std::istream_iterator<std::string>{}};
-
-        double x, y, z;
-        try {
-            auto it = record_positions.find("model_Cartn_x");
-            x = std::stod(records[it->second]);
-
-            it = record_positions.find("model_Cartn_y");
-            y = std::stod(records[it->second]);
-
-            it = record_positions.find("model_Cartn_z");
-            z = std::stod(records[it->second]);
-        } catch (std::exception &) {
-            fmt::print(stderr, "Error reading coordinates.\n");
-            exit(EXIT_FILE_ERROR);
-        }
-
-        auto it = record_positions.find("type_symbol");
-        auto symbol = records[it->second];
-        auto element = PeriodicTable::pte().get_element_by_symbol(get_element_symbol(symbol));
-
-        it = record_positions.find("atom_id");
-        auto atom_name = fix_atom_name(records[it->second]);
-
-        int residue_id = 0;
-
-        it = record_positions.find("comp_id");
-        auto residue = records[it->second];
-
-        atoms->emplace_back(idx, element, x, y, z, atom_name, residue_id, residue, "0", false);
-        idx++;
-
-        std::getline(file, line);
-        boost::trim(line);
-    } while (line[0] != '#');
-
-    auto bonds = get_bonds(atoms);
-    std::map<size_t, int> charges;
-    molecules->emplace_back(name, std::move(atoms), std::move(bonds), charges);
 }
