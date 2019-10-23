a93315ae2ad19c996ebeca98b5cbc55ec76cf0e3
Tomáš Raček <tom@krab1k.net>
2019-10-23 12:48:45 +0200

    Update information about the bonds from CCD

--- utils/ccd_bond_order.py
+++ utils/ccd_bond_order.py
@@ -4,38 +4,64 @@
 
 import sys
 
-with open(sys.argv[1]) as f:
-    data = f.read()
 
-order = {'SING': 1, 'DOUB': 2, 'TRIP': 3}
+AMINO_ACIDS = {'ALA', 'ARG', 'ASN', 'ASP', 'CYS', 'GLN', 'GLU', 'GLY', 'HIS', 'ILE', 'LEU', 'LYS', 'MET', 'PHE', 'PRO',
+               'SER', 'THR', 'TRP', 'TYR', 'VAL'}
+
+ORDER = {'SING': 1, 'DOUB': 2, 'TRIP': 3}
 
 
-it = iter(data.split('\n'))
+def set_file(residue_name: str):
+    return amino_acids_file if residue_name in AMINO_ACIDS else other_file
 
-try:
-    while True:
-        line = next(it)
-        if line.startswith('#'):
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print('Not enough arguments', file=sys.stderr)
+        exit(1)
+
+    amino_acids_filename = sys.argv[2]
+    other_filename = sys.argv[3]
+
+    amino_acids_file = open(amino_acids_filename, 'w')
+    other_file = open(other_filename, 'w')
+
+    with open(sys.argv[1]) as f:
+        data = f.read()
+
+    it = iter(data.split('\n'))
+
+    file = None
+    try:
+        while True:
             line = next(it)
-            if line.startswith('loop_'):
+            if line.startswith('#'):
                 line = next(it)
-                if line.startswith('_chem_comp_bond'):
-                    while line.startswith('_'):
-                        line = next(it)
-                    print(line.split()[0])
-                    while not line.startswith('#'):
-                        atom1, atom2, b_order = line.split()[1: 4]
-                        b_order = order[b_order]
-                        print(f'{atom1:s} {atom2:s} {b_order:d}')
-                        line = next(it)
-                    print()
-            else:
-                if line.startswith('_chem_comp_bond'):
-                    print(line.split()[1])
-                    atom1 = next(it).split()[1]
-                    atom2 = next(it).split()[1]
-                    b_order = order[next(it).split()[1]]
-                    print(f'{atom1:s} {atom2:s} {b_order:d}')
-                    print()
-except StopIteration:
-    pass
+                if line.startswith('loop_'):
+                    line = next(it)
+                    if line.startswith('_chem_comp_bond'):
+                        while line.startswith('_'):
+                            line = next(it)
+                        name = line.split()[0]
+                        file = set_file(name)
+                        print(name, file=file)
+                        while not line.startswith('#'):
+                            atom1, atom2, b_order = line.split()[1: 4]
+                            b_order = ORDER[b_order]
+                            print(f'{atom1:s} {atom2:s} {b_order:d}', file=file)
+                            line = next(it)
+                        print(file=file)
+                else:
+                    if line.startswith('_chem_comp_bond'):
+                        name = line.split()[1]
+                        file = set_file(name)
+                        print(name, file=file)
+                        atom1 = next(it).split()[1]
+                        atom2 = next(it).split()[1]
+                        b_order = ORDER[next(it).split()[1]]
+                        print(f'{atom1:s} {atom2:s} {b_order:d}', file=file)
+                        print(file=file)
+    except StopIteration:
+        pass
+    amino_acids_file.close()
+    other_file.close()
