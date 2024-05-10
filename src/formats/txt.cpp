96768a1f7d18ed4debc7e67010ce531d00147dbc
Tomáš Raček <tom@krab1k.net>
2024-05-10 09:49:47 +0200

    Remove custom formatting in favor of fmt

--- src/formats/txt.cpp
+++ src/formats/txt.cpp
@@ -5,25 +5,24 @@
 #include <fstream>
 #include <string>
 #include <fmt/format.h>
+#include <fmt/os.h>
 
 #include "chargefw2.h"
 #include "../charges.h"
 #include "txt.h"
-#include "../utility/utility.h"
 #include "../utility/strings.h"
 
 
 void TXT::save_charges(const MoleculeSet &, const Charges &charges, const std::string &filename) {
-    std::ofstream file(filename);
-    if (!file) {
+    try {
+        auto file = fmt::output_file(filename);
+        for (const auto &name: charges.names()) {
+            file.print("{}\n", to_uppercase(name));
+            file.print("{:.5f}\n", fmt::join(charges[name], " "));
+        }
+        file.close();
+    } catch (std::system_error &e) {
         fmt::print(stderr, "Cannot open file: {}\n", filename);
         exit(EXIT_FILE_ERROR);
     }
-
-    for (const auto &name: charges.names()) {
-        file << to_uppercase(name) << std::endl;
-        file << charges[name] << std::endl;
-    }
-
-    file.close();
 }
