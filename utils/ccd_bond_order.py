5a1fffeb661e40dd9b78b5f76310505304681dd8
Tomáš Raček <tom@krab1k.net>
2025-12-04 07:49:11 +0100

    Update bond info

--- utils/ccd_bond_order.py
+++ utils/ccd_bond_order.py
@@ -1,67 +1,51 @@
-#!/usr/bin/env python3.7
+#!/usr/bin/env python3
 
 # Convert bond order information from CCD's data to a more compact format
 
-import sys
+import argparse
+import gemmi
+
+AMINO_ACIDS = {
+    'ALA', 'ARG', 'ASN', 'ASP', 'CYS', 'GLN', 'GLU', 'GLY', 'HIS',
+    'ILE', 'LEU', 'LYS', 'MET', 'PHE', 'PRO', 'SER', 'THR', 'TRP',
+    'TYR', 'VAL',
+}
+
+# Map gemmi bond types to integer orders
+BOND_ORDER = {
+    gemmi.BondType.Single: 1,
+    gemmi.BondType.Double: 2,
+    gemmi.BondType.Triple: 3,
+}
 
 
-AMINO_ACIDS = {'ALA', 'ARG', 'ASN', 'ASP', 'CYS', 'GLN', 'GLU', 'GLY', 'HIS', 'ILE', 'LEU', 'LYS', 'MET', 'PHE', 'PRO',
-               'SER', 'THR', 'TRP', 'TYR', 'VAL'}
+def main():
+    parser = argparse.ArgumentParser(
+        description='Convert CCD bond order information to a compact format.'
+    )
+    parser.add_argument('ccd_file', help='CCD file (components.cif or components.cif.gz)')
+    parser.add_argument('amino_acids_out', help='Output file for amino-acid residues')
+    parser.add_argument('other_out', help='Output file for non-amino-acid residues')
 
-ORDER = {'SING': 1, 'DOUB': 2, 'TRIP': 3}
+    args = parser.parse_args()
+    doc = gemmi.cif.read(args.ccd_file)
 
+    with open(args.amino_acids_out, 'w') as aa_out, open(args.other_out, 'w') as other_out:
+        for block in doc:
+            cc = gemmi.make_chemcomp_from_block(block)
+            resname = cc.name
 
-def set_file(residue_name: str):
-    return amino_acids_file if residue_name in AMINO_ACIDS else other_file
+            out_file = aa_out if resname in AMINO_ACIDS else other_out
+            print(resname, file=out_file)
+
+            for bond in cc.rt.bonds:
+                order = BOND_ORDER.get(bond.type)
+                atom1 = bond.id1.atom
+                atom2 = bond.id2.atom
+                print(f'{atom1} {atom2} {order}', file=out_file)
+
+            print(file=out_file)  # blank line after each residue
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print('Not enough arguments', file=sys.stderr)
-        exit(1)
-
-    amino_acids_filename = sys.argv[2]
-    other_filename = sys.argv[3]
-
-    amino_acids_file = open(amino_acids_filename, 'w')
-    other_file = open(other_filename, 'w')
-
-    with open(sys.argv[1]) as f:
-        data = f.read()
-
-    it = iter(data.split('\n'))
-
-    file = None
-    try:
-        while True:
-            line = next(it)
-            if line.startswith('#'):
-                line = next(it)
-                if line.startswith('loop_'):
-                    line = next(it)
-                    if line.startswith('_chem_comp_bond'):
-                        while line.startswith('_'):
-                            line = next(it)
-                        name = line.split()[0]
-                        file = set_file(name)
-                        print(name, file=file)
-                        while not line.startswith('#'):
-                            atom1, atom2, b_order = line.split()[1: 4]
-                            b_order = ORDER[b_order]
-                            print(f'{atom1:s} {atom2:s} {b_order:d}', file=file)
-                            line = next(it)
-                        print(file=file)
-                else:
-                    if line.startswith('_chem_comp_bond'):
-                        name = line.split()[1]
-                        file = set_file(name)
-                        print(name, file=file)
-                        atom1 = next(it).split()[1]
-                        atom2 = next(it).split()[1]
-                        b_order = ORDER[next(it).split()[1]]
-                        print(f'{atom1:s} {atom2:s} {b_order:d}', file=file)
-                        print(file=file)
-    except StopIteration:
-        pass
-    amino_acids_file.close()
-    other_file.close()
+    main()
